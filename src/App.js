32cd8d81a625b9afecf401b8b3cbd92036c46058
jeco18 <bongayjericho@gmail.com>
2022-08-04 21:25:06 +0800

    appjs revision

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import {
 import './App.css';
 import GetClientToken from './api/getClientToken';
 import GetAdminToken from './api/getAdminToken';
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 
 function App() {
   let token = GetClientToken();
 
   console.log(token);
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
